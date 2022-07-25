30d9e983a8b0a360609e2d23afbe154c5d787ead
Batiazinga <luc.nguyen-the@pm.me>
2022-07-25 20:26:25 +0200

    Augment benchmarks

--- bench_pkg/bench.py
+++ bench_pkg/bench.py
@@ -1,6 +1,21 @@
 from .func import do_something
 
 
-def test_do_something(benchmark):
+def test_do_something_00001(benchmark):
+    result = benchmark(do_something, duration=0.00001)
+    assert result
+
+
+def test_do_something_0001(benchmark):
+    result = benchmark(do_something, duration=0.0001)
+    assert result
+
+
+def test_do_something_001(benchmark):
     result = benchmark(do_something, duration=0.001)
+    assert result
+
+
+def test_do_something_01(benchmark):
+    result = benchmark(do_something, duration=0.01)
     assert result
--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -9,11 +9,23 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     set_python_path(".").unwrap();
     let caller = Caller::init().unwrap();
 
+    c.bench_function("0.00001", |b| b.iter(|| caller.do_something(0.00001)));
+    c.bench_function("0.0001", |b| b.iter(|| caller.do_something(0.0001)));
     c.bench_function("0.001", |b| b.iter(|| caller.do_something(0.001)));
+    c.bench_function("0.01", |b| b.iter(|| caller.do_something(0.01)));
 
     Python::with_gil(|py| {
+        c.bench_function("with_gil 0.00001", |b| {
+            b.iter(|| caller.do_something_with_gil(py, 0.00001))
+        });
+        c.bench_function("with_gil 0.0001", |b| {
+            b.iter(|| caller.do_something_with_gil(py, 0.0001))
+        });
         c.bench_function("with_gil 0.001", |b| {
             b.iter(|| caller.do_something_with_gil(py, 0.001))
+        });
+        c.bench_function("with_gil 0.01", |b| {
+            b.iter(|| caller.do_something_with_gil(py, 0.01))
         });
     })
 }
